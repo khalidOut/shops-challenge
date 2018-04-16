e5ff1c881380be6ee713c97225700e8f1d2996af
khalid <khalid@tudioweb.com>
2018-04-16 21:18:13 +0100

    init api with register and login endpoints

---
+++ api/src/Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Controller used to manage the application security.
+ * See https://symfony.com/doc/current/cookbook/security/form_login_setup.html.
+ *
+ * @author Ryan Weaver <weaverryan@gmail.com>
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/api/getToken")
+     * @Method({"POST"})
+     */
+    public function getTokenAction()
+    {
+        // The security layer will intercept this request
+        return new Response('', 401);
+    }
+}
---
+++ api/src/Service/UserManager.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Utils\Validator;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserManager
+{
+    private $entityManager;
+    private $passwordEncoder;
+    private $validator;
+    private $users;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $encoder, Validator $validator, UserRepository $users)
+    {
+        $this->entityManager = $em;
+        $this->passwordEncoder = $encoder;
+        $this->validator = $validator;
+        $this->users = $users;
+    }
+
+    public function create($email, $plainPassword)
+    {
+        // make sure to validate the user data is correct
+        $this->validateUserData($email, $plainPassword);
+
+        // create the user and encode its password
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles(['ROLE_USER']);
+
+        $encodedPassword = $this->passwordEncoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encodedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
+    }
+
+    private function validateUserData($email, $plainPassword)
+    {
+        // validate password and email if is not this input means interactive.
+        $this->validator->validateEmail($email);
+        $this->validator->validatePassword($plainPassword);
+
+        // check if a user with the same email already exists.
+        $existingEmail = $this->users->findOneBy(['email' => $email]);
+
+        if (null !== $existingEmail) {
+            throw new RuntimeException(sprintf('There is already a user registered with the "%s" email.', $email));
+        }
+    }
+}
